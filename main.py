fed50a24b2ab3bbb109cd15f32eb1a62be3371f4
Chase <100978029+ChaseRocks360@users.noreply.github.com>
2024-07-16 16:39:43 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import time
-import ctypes
 from PIL import ImageGrab
 import win32api
 import win32con
+import pyautogui
+import keyboard  # Import the keyboard library
 
 # Function to capture the screen
 def capture_screen():
@@ -24,7 +25,12 @@ def contains_orange_block(image, target_color=(224, 136, 29)):
 no_orange_block_time = 0
 last_checked = time.time()
 
+# Main loop
 while True:
+    if keyboard.is_pressed('F4'):  # Check if F4 is pressed
+        print("F4 pressed. Exiting script.")
+        break
+
     image = capture_screen()
 
     if contains_orange_block(image):
@@ -33,10 +39,22 @@ while True:
         no_orange_block_time += 1
 
     if no_orange_block_time >= 5:
+        # Wait 5 seconds
+        time.sleep(5)
+
+        # Press "T" key
+        pyautogui.press('t')
+
+        # Type "/garden"
+        pyautogui.typewrite('/garden')
+        pyautogui.press('enter')
+
         # Press F8
         win32api.keybd_event(win32con.VK_F8, 0, 0, 0)
         win32api.keybd_event(win32con.VK_F8, 0, win32con.KEYEVENTF_KEYUP, 0)
-        no_orange_block_time = 0  # Reset the timer after pressing F8
+
+        # Reset the timer after typing the command and pressing F8
+        no_orange_block_time = 0
 
     # Check the screen every second
     while time.time() - last_checked < 1:
